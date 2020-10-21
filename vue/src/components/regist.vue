4de4fcf7162ba98edcc2f3fd9c6cf406c8e2b916
Unknown <unknown@example.com>
2020-10-21 13:58:20 +0900

    登録ページを見やすくした＆戻るボタン追加

--- vue/src/components/regist.vue
+++ vue/src/components/regist.vue
@@ -1,46 +1,55 @@
 <template>
-    <div class="regist">
-      <form @submit.prevent="regist">
-        <table>
-          <thead></thead>
-          <tbody>
-            <tr>
-              <th>
-                <label for="name">名前</label>
-              </th>
-              <th>
-                <input type="text" id="name" v-model="frm.Name" required>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <label for="pass">パスワード</label>
-              </th>
-              <th>
-                <input type="text" v-model="frm.Password" required readonly>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <label for="strNum">学籍番号</label>
-              </th>
-              <th>
-                <input type="text" id="strNum" pattern="[a-z]\d{5}" :value="frm.Studentnumber" @input="frm.Studentnumber = $event.target.value;onChangeStuNum();" maxlength="6" required>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <label for="Email">メアド</label>
-              </th>
-              <th>
-                <input type="email" id="email" v-model="frm.Email" maxlength='45' required readonly>
-              </th>
-            </tr>
-          </tbody>
-        </table>
-        <input class="bt" type="submit" value="登録">
-      </form>
-      {{frm}}
+    <div class="registContainer">
+      <div class="regist">
+        <div>
+          <div class="backBt" v-on:click="hideRegist"></div>
+        </div>
+        <form @submit.prevent="regist">
+          <table>
+            <thead>
+              <tr>
+                <th>入室時間</th>
+                <th>{{time}}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>
+                  <label for="name">名前</label>
+                </th>
+                <th>
+                  <input type="text" id="name" v-model="frm.Name" required>
+                </th>
+              </tr>
+              <tr>
+                <th>
+                  <label for="pass">パスワード</label>
+                </th>
+                <th>
+                  <input type="text" v-model="frm.Password" required readonly>
+                </th>
+              </tr>
+              <tr>
+                <th>
+                  <label for="strNum">学籍番号</label>
+                </th>
+                <th>
+                  <input type="text" id="strNum" pattern="[a-z]\d{5}" :value="frm.Studentnumber" @input="frm.Studentnumber = $event.target.value;onChangeStuNum();" maxlength="6" required>
+                </th>
+              </tr>
+              <tr>
+                <th>
+                  <label for="Email">メアド</label>
+                </th>
+                <th>
+                  <input type="email" id="email" v-model="frm.Email" maxlength='45' required readonly>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+          <input class="bt" type="submit" value="登録">
+        </form>
+      </div>
     </div>
 </template>
 
@@ -71,6 +80,10 @@ export default {
     onChangeStuNum(){
       var gstr=this.frm.Studentnumber.slice(0,1);
       this.frm.Email=this.frm.Studentnumber +gstr+gstr+"@aitech.ac.jp";
+    },
+    hideRegist(){
+      this.frm.UID=null;
+      this.$parent.uid=null;
     }
   },
   mounted(){
@@ -80,14 +93,22 @@ export default {
 }
 </script>
 <style>
+  .registContainer{
+    left: 0;
+    width: 100%;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+  }
   .regist{
-    margin-top: 2rem;
+    box-shadow: 5px 10px 10px #0BB;
     background-color: #9EE;
     border-radius: 1rem;
-    padding: 1.5rem 1rem;
+    padding: 0.75rem 1rem 1.5rem;
     display: flex;
     justify-content: center;
     flex-direction: column;
+    z-index: 2;
   }
   .regist .bt{
     color: #EFF;
@@ -104,5 +125,13 @@ export default {
   .regist .bt:hover{
     background-color: #0BD;
     border-color: #09D;
+  }
+  .backBt{
+    height: 1rem;
+    width: 1rem;
+    border-radius: 50%;
+    background-color: #5CC;
+    margin-left: auto;
+    margin-bottom: 0.5rem;
   }
 </style>
