8ce3ab268716deef6371c9719ff77772ab6cf76a
Unknown <unknown@example.com>
2020-10-23 13:07:10 +0900

    vueが8081でリクエストしてなかったんで直した

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -16,7 +16,7 @@ export default {
   name: 'App',
   data: () => ({
     // apiのホストアドレス
-    host: 'http://172.16.6.4',
+    host: 'http://172.16.6.4:8081',
     // host: 'http://localhost:8081',
     view:'log',
   }),
